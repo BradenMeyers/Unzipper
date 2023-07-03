b468cfcce6e892a00034dcd20d6b3fbba21cdb8f
ktownkinley <ktownkinley@gmail.com>
2023-07-02 20:59:43 -0600

    Kinley Commit

--- src/gps.cpp
+++ src/gps.cpp
@@ -27,7 +27,7 @@ void loopGPS(){
       Serial.print(gps.location.lat(), 6);
       Serial.print(" Longitude= "); 
       Serial.println(gps.location.lng(), 6);
-      // Number of satellites in use (u32)
+      // Number of satellites in use (u32) hi
       Serial.print("Number of satellites in use = "); 
       Serial.println(gps.satellites.value());
 
